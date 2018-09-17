40e42f45e02ed0bc36729122f61dab7899a2f487
rubypruneda <rubypruneda@outlook.com>
2018-09-16 20:53:24 -0600

    working on calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,11 +1,23 @@
 <template>
-    
+    <div>
+        <p>
+            is it working?
+        </p>
+        <div class = 'calculator'>
+            
+        </div>
+    </div>
 </template>
 
 <script>
     
 </script>
 
-<style>
-    
+<style >
+    .calculator {
+        height: 450px;
+        width: 330px;
+        background-color: skyblue;
+        margin: auto;
+    }
 </style>
