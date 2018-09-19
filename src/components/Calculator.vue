2f778c0abc504e73918ddd3cdf87705d6e31accd
rubypruneda <rubypruneda@outlook.com>
2018-09-18 21:16:57 -0600

    looks good

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,55 +4,79 @@
             is it working?
         </p>
         <div class = 'calculator'>  
-            <div class = 'display-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
-            <div class = 'little-box'></div>    
+            <div class = 'display' style = 'border-radius: 10px 10px'>{{now || 0}}</div>
+            <div v-on:click="clear" class = 'button'>AC</div>    
+            <div class = 'button'>+/-</div>    
+            <div class = 'button'>%</div>    
+            <div class = 'button orange'>÷</div>    
+            <div class = 'button'>7</div>    
+            <div class = 'button'>8</div>    
+            <div class = 'button'>9</div>    
+            <div class = 'button orange'>*</div>    
+            <div class = 'button'>4</div>    
+            <div class = 'button'>5</div>    
+            <div class = 'button'>6</div>    
+            <div class = 'button orange'>-</div>    
+            <div class = 'button'>1</div>    
+            <div class = 'button'>2</div>    
+            <div class = 'button'>3</div>    
+            <div class = 'button orange'>+</div>    
+            <div class = 'button zero'>0</div>    
+            <div class = 'button'>.</div>    
+            <div class = 'button orange' style = 'border-radius: 0 0 10px'>=</div>    
         </div>
     </div>
 </template>
 
 <script>
-    
+    export default{
+        data() {
+            return {
+                now: ';lkj',
+            }
+        },
+        methods: {
+            clear () {
+                this.now = '';
+            }
+        }
+    }
 </script>
 
 <style >
     .calculator {
-        height: 450px;
-        width: 330px;
-        background-color: skyblue;
+       display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(50px, auto);
+        background-color: lightgrey;
+        width: 260px;
+        height: 350px;
         margin: auto;
-        padding-top: 10px;
+        border-radius: 10px;
     }
 
-    .little-box {
-        height: 62px;
-        width: 62px;
-        display: grid;
-        background-color: pink;
-        margin: 10px;
-         grid-row: auto / auto;
-         float: left;
+    .display { 
+        grid-column: 1/5;
+        background-color: lightblue;
     }
 
-    .display-box {
-        height: 60px;
-        width: 90%;
-        background-color: yellow;
-        margin: auto;
-        margin-bottom: 20px;
+    .zero {
+        grid-column: 1/3;
+        border-radius: 0 0 0 10px;
     }
+
+    .button {
+        padding-top: 13px;
+        border: 1px solid grey; 
+        font-size: 23px;
+        font-weight: 500;
+    }
+
+    .orange { 
+        background-color: orange;
+        color: white;
+        font-size: 35px;
+        font-weight: 200;
+    }
+
 </style>
