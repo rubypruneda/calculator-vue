99e652a490d0dfc5d1d69006c49fdf6631d92217
rubypruneda <rubypruneda@outlook.com>
2018-09-23 20:35:13 -0600

    mostly functioning

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,8 +1,5 @@
 <template>
-    <div>
-        <p>
-            is it working?
-        </p>
+    <div class = 'center'>
         <div class = 'calculator'>  
             <div class = 'display' style = 'border-radius: 10px 10px 0 0'>{{now || 0}}</div>
             <div v-on:click="clear" class = 'button'>AC</div>    
@@ -12,11 +9,11 @@
             <div v-on:click="number('7')" class = 'button'>7</div>    
             <div v-on:click="number('8')" class = 'button'>8</div>    
             <div v-on:click="number('9')" class = 'button'>9</div>    
-            <div class = 'button orange'>*</div>    
+            <div v-on:click="multiply" class = 'button orange'>*</div>    
             <div v-on:click="number('4')" class = 'button'>4</div>    
             <div v-on:click="number(5)" class = 'button'>5</div>    
             <div v-on:click="number('6')" class = 'button'>6</div>    
-            <div class = 'button orange'>-</div>    
+            <div v-on:click="minus" class = 'button orange'>-</div>    
             <div v-on:click="number('1')" class = 'button'>1</div>    
             <div v-on:click="number('2')" class = 'button'>2</div>    
             <div v-on:click="number('3')" class = 'button'>3</div>    
@@ -34,8 +31,9 @@
         data() {
             return {
                 now: '',
-                hold: '',
-                operator: null
+                hold: null,
+                operator: null,
+                opClicked: false
             }
         },
         methods: {
@@ -61,17 +59,35 @@
                 // console.log(this.hold)
                 this.operator = (a, b) => a + b;
                 this.hold = this.now;
-
+                this.now = '';
+                this.opClicked = true;
+            }, 
+            minus() {
+                this.operator = (a, b) => b - a;
+                this.hold = this.now;
+                this.now = '';
+                this.opClicked = true;
+            },
+            multiply() {
+                this.operator = (a, b) => a * b;
+                this.hold = this.now;
+                this.now = '';
+                this.opClicked = true;
             },
             equal() {
-               this.now = +this.now + +this.hold;
+                this.now = `${this.operator(+this.now , +this.hold)}`
                 console.log(this.now)
+                this.hold = null;
             }
         }
     } 
 </script>
 
 <style >
+    .center {
+        padding-top: 15%;
+    }
+
     .calculator {
        display: grid;
         grid-template-columns: repeat(4, 1fr);
@@ -79,8 +95,9 @@
         background-color: lightgrey;
         width: 260px;
         height: 350px;
-        margin: auto;
+        margin:  auto;
         border-radius: 10px;
+        top: 50%;
     }
 
     .display { 
